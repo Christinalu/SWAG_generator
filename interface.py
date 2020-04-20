4ad3124ddd1869db61d3b6507ff81279f1842ca7
Christina <xza172@sfu.ca>
2020-04-19 19:14:03 -0700

    Added read from web function

--- interface.py
+++ interface.py
@@ -1,29 +1,61 @@
 from tkinter import *
 from tkinter import filedialog
-
+from tkinter import messagebox
 from tika import parser # you will need java 8 enviornment to run this
 
+import bs4 as bs
+import urllib.request
+import re
+    
+url_text = ''
 
-class GUI:
-    def __init__(self, window):
-        self.window = window
-        window.geometry('500x400')
-        fm = Frame(window)
-        Label(fm, text="Welcome to SAWG generator!", height=8, font = ("Consolas")).pack(side=TOP)
-        Button(fm, text="Upload PDF file", command=self.uploadFile, font=("Consolas",12), bd=1, bg="black", fg="white").pack(side=TOP, expand=YES)
-        fm.pack(fill=BOTH, expand=YES)
-        
+def uploadFile():
+    # NOTE: Now we accept .pdf and .txt file to upload, extract its words to a full "string" paragraph.
+    fileRead = filedialog.askopenfile(title = "Select file", filetypes= (("PDF files","*.pdf"),("Text files","*.txt")))
+    raw_content = parser.from_file(fileRead.name)
 
-    def uploadFile(self):
-        # NOTE: Now we accept .pdf and .txt file to upload, extract its words to a full "string" paragraph.
-        fileRead = filedialog.askopenfile(title = "Select file", filetypes= (("PDF files","*.pdf"),("Text files","*.txt")))
-        # all file content is now store in "raw_content", TODO: how to deal with those paragraphs with NLTK?
-        raw_content = parser.from_file(fileRead.name)
+    # print(raw_content['content'])
+    return raw_content['content']
 
-        # print(raw_content['content'])
+def readFromWeb():
+    # Need to install BeautifulSoup 4, lxml
+    global url_text
+    web_url = str(url_text.get())
+    print(url_text)
+    print(web_url)
+    extracted_article = ''
 
-        
-if __name__ == "__main__":
-    root = Tk(className=' SWAG generator')    
-    display = GUI(root)    
-    root.mainloop()
+    if web_url != '':
+        try:
+            website = urllib.request.urlopen(web_url).read()
+
+            soup = bs.BeautifulSoup(website, 'lxml')
+            extracted_article = ""
+            for item in soup.find_all ('p'):
+                extracted_article = extracted_article + item.text
+
+            extracted_article = re.sub(r'\s+',' ',extracted_article)
+
+            extracted_article = re.sub(r'\[[0-9]*\]',' ',extracted_article)
+        except:
+            messagebox.showwarning(title="Input Error", message="URL Error, please re-enter.")
+
+    # print (extracted_article)
+    return extracted_article
+
+
+def playGUI():
+    window = Tk(className=' SWAG generator')  
+    window.geometry('500x400')
+    fm = Frame(window)
+    fm.pack(fill=BOTH)
+    Label(fm, text="Welcome to SAWG generator!", height=7, font = ("Consolas")).pack(side=TOP)
+    Label(fm, text="Website URL:", font=("Consolas",13)).pack(side = LEFT, expand = True)
+    global url_text
+    url_text = Entry(fm, width=40, bd=2)
+    url_text.pack(side = LEFT, expand = True)
+    Button(fm, text="GO!", command=readFromWeb, font=("Consolas",12), bd=1, bg="black", fg="white").pack(side = LEFT, expand = True)
+    Label(window, text="or", height=2, font=("Consolas",13)).pack()
+    Button(window, text="Upload PDF/txt file", command=uploadFile, font=("Consolas",12), bd=1, bg="black", fg="white").pack()
+    
+    window.mainloop()
---
+++ main.py
@@ -0,0 +1,5 @@
+import interface
+
+if __name__ == '__main__':
+    interface.playGUI()
+    # TODO: combine the summarizor and NTLK
